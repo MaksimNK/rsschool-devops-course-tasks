36bedd024459fde06a041a395c8d5c087984ad63
MaksimNK <maksmozir@gmail.com>
2025-06-21 23:30:12 +0300

    feat: add new infrastructure

---
+++ routes.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public" {
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private" {
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
