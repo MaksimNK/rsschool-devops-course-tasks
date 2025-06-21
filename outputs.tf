0aba81a677fcc9351843e3cec380b2ba252d53c6
MaksimNK <maksmozir@gmail.com>
2025-06-21 23:28:56 +0300

    feat: add new var and output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
-output "name" {
-  value       = "terraform-states-2025-rs"
-  sensitive   = true
-  description = "aws terrafrom output"
-  depends_on  = []
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "VPC ID"
+}
+output "public_subnet_ids" {
+  value       = aws_subnet.public[*].id
+  description = "IDs of public subnets"
+}
+output "private_subnet_ids" {
+  value       = aws_subnet.private[*].id
+  description = "IDs of private subnets"
+}
+output "bastion_public_ip" {
+  value       = aws_instance.bastion.public_ip
+  description = "Public IP of bastion host"
 }
